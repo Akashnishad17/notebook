a810e33ee33936f33e83574be79b90fad74d340f
Akash Nishad <akashnishad2017@gmail.com>
2023-03-04 22:53:33 +0530

    login API

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -22,7 +22,7 @@ router.post('/createuser',
         try {
             const existingUser = await User.findOne({email : req.body.email});
             if(existingUser){
-                return res.status(400).json({errors: 'User with this email already exists.'});
+                return res.status(400).json({errors: 'User with this email already exists'});
             }
 
             const salt = await bcrypt.genSalt(10);
@@ -39,12 +39,51 @@ router.post('/createuser',
                     id: user.id
                 }
             };
-            
+
             const token = jwt.sign(data, JWT_SECRET_KEY);
             res.json(token);
         } catch(error) {
             console.error(error.message);
-            res.status(500).send("Internal Server Error");
+            res.status(500).send('Internal Server Error');
+        }
+    }
+);
+
+router.post('/login',
+    [
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', 'Password can not be blank').exists()
+    ],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        
+        try{
+            const {email, password} = req.body;
+            const user = await User.findOne({email: email});
+
+            if(!user){
+                return res.status(400).json({errors: 'Please enter correct email and password'});
+            }
+
+            const isCorrectPassword = await bcrypt.compare(password, user.password);
+            if(!isCorrectPassword){
+                return res.status(400).json({errors: 'Please enter correct email and password'});
+            }
+
+            const data = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            const token = jwt.sign(data, JWT_SECRET_KEY);
+            res.json(token);
+        }catch(error){
+            console.error(error.message);
+            res.status(500).send('Internal Server Error');
         }
     }
 );
